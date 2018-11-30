4e75f847a8ce2921d5968158989e8ed2a2011c50
ladaegorova18 <st068843@student.spbu.ru>
2018-11-30 18:31:03 +0300

    task 3.1

---
+++ sem1/tests/3.1/3.1/options.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "options.h"
+using namespace std;
+
+bool isEmpty(DynList *myList)
+{
+	return (myList->head == nullptr);
+}
+
+void insertion(DynList *myList, int newValue)
+{
+	myList->size++;
+	Element* data = new Element;
+	data->next = myList->head;
+	data->value = newValue;
+	if (myList->head != nullptr)
+	{
+		myList->tail->next = data;
+		myList->tail = data;
+	}
+	else
+	{
+		myList->head = data;
+		myList->tail = data;
+	}
+}
+
+void deleteList(DynList *myList)
+{
+	Element *current = myList->head;
+	Element *previous = nullptr;
+	while (current)
+	{
+		previous = current;
+		current = current->next;
+		delete previous;
+	}
+	delete myList;
+}
