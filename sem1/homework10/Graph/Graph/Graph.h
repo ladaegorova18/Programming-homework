1f805155224f7cc90976be5409eb29369267dd8f
ladaegorova18 <st068843@student.spbu.ru>
2018-12-11 09:43:42 +0300

    created project, added task files and test functions

--- sem1/homework10/Graph/Graph/Graph.h
+++ sem1/homework10/Graph/Graph/Graph.h
@@ -1,2 +1,22 @@
 #pragma once
+#include <vector>
+#include <iostream>
+const int INF = 1000000000;
 
+struct Map
+{
+	// массив расстояний между городами
+	std::vector< std::vector<int> > roads;
+
+	// kingdoms[i] массив государств; в каждом есть список владений, принадлежащих им
+	std::vector< std::vector<int> > kingdoms;
+
+	// отмечаем здесь города, в которых были
+	std::vector <bool> mark;
+
+	// раздел территории
+	void war();
+};
+
+// вывод на экран королевств и их владений
+void printing(std::vector< std::vector<int> > kingdoms);
--- sem1/homework10/Graph/Graph/main.cpp
+++ sem1/homework10/Graph/Graph/main.cpp
@@ -3,30 +3,76 @@
 #include <iostream>
 #include <fstream>
 #include <locale>
+#include <assert.h>
+
+void reading(std::ifstream &file, Map* map)
+{
+	if (!file)
+	{
+		std::cout << "Файл не найден!" << std::endl;
+		return;
+	}
+	int n = 0; // cities
+	int m = 0; // roads
+	file >> n;
+	file >> m;
+	map->mark.resize(n);
+	map->roads.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		map->mark[i] = false;
+		map->roads[i].resize(n);
+		for (int j = 0; j < n; j++)
+		{
+			map->roads[i][j] = INF;
+		}
+	}
+	while (!file.eof())
+	{
+		for (int a = 0; a < m; a++)
+		{
+			int i = 0;
+			int j = 0;
+			int len = 0;
+			file >> i >> j >> len;
+			map->roads[i][j] = len;
+			map->roads[j][i] = len;
+		}
+		int k = 0; // capitals
+		file >> k;
+		map->kingdoms.resize(k);
+		for (int i = 0; i < k; i++)
+		{
+			int capitalNumber = 0;
+			file >> capitalNumber;
+			map->kingdoms[i].push_back(capitalNumber);
+			map->mark[capitalNumber] = 1; // столица уже кому-то принадлежит
+		}
+	}
+}
+
+void test()
+{
+	std::ifstream testFile("testFile.txt");
+	Map* testMap = new Map();
+	reading(testFile, testMap);
+	testMap->war();
+
+	testFile.close();
+	std::cout << "Тест пройден!" << std::endl;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	test();
+	Map* map = new Map();
 	std::ifstream file("kingdoms.txt");
-	if (!file)
-	{
-		std::cout << "Файл не найден!" << std::endl;
-		return -1;
-	}
-	int n = 0; // cities
-	int m = 0; // roads
-	while (!file.eof())
-	{
-		int i = 0;
-		int j = 0;
-		int len = 0;
-		std::cin >> i >> j >> len;
-
-	}
-	std::cout << "Введите число столиц: " << std::endl;
-	int k = 0;
-	std::cin >> k;
-
-
+	reading(file, map);
+	file.close();
+	map->war();
+	printing(map->kingdoms);
+	std::cout << "До свидания!" << std::endl;
+	system("pause");
 	return 0;
 }
