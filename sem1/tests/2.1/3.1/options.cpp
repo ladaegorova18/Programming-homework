f213e4bf52d36239669613d8c0b81efe58011bff
ladaegorova18 <st068843@student.spbu.ru>
2018-12-13 15:53:43 +0300

    добавлены задачи из второго теста

---
+++ sem1/tests/2.1/3.1/options.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "options.h"
+using namespace std;
+
+bool isEmpty(DynList *myList)
+{
+	return (myList->head == nullptr);
+}
+
+void insertion(DynList *myList, int newValue)
+{
+	myList->size++;
+	Element* data = new Element;
+	data->next = myList->head;
+	data->value = newValue;
+	if (myList->head != nullptr)
+	{
+		myList->tail->next = data;
+		myList->tail = data;
+	}
+	else
+	{
+		myList->head = data;
+		myList->tail = data;
+	}
+}
+
+void deleteList(Element* head)
+{
+	while (head != nullptr)
+	{
+		Element *previous = head;
+		head = head->next;
+		delete previous;
+	}
+	return;
+}
