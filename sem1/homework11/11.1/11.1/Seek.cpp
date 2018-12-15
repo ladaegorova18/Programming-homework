cbe7a579a874983e486a7fb974b157724ea5ff7a
ladaegorova18 <st068843@student.spbu.ru>
2018-12-15 18:46:02 +0300

    доделаны тесты, возможно, нужно модернизировать функцию поиска

---
+++ sem1/homework11/11.1/11.1/Seek.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "Seek.h"
+
+int* prefixFunction(std::string str, int len)
+{
+	int *index = new int[len] {0};
+	for (int i = 0; i < len - 1; i++)
+	{
+		if (str[i + 1] == str[index[i]])
+		{
+			index[i + 1] = index[i] + 1;
+		}
+		else
+		{
+			index[i + 1] = 0;
+		}
+	}
+	return index;
+}
+
+int algorithmKMP(std::string main, std::string sample)
+{
+	int sampleLen = sample.length();
+	int mainLen = main.length();
+	int equalIndex = 0;
+	int *index = prefixFunction(sample, sampleLen);
+	for (int i = 0; i < mainLen; )
+	{
+		if (equalIndex == sampleLen)
+		{
+			return i - equalIndex + 1;
+		}
+		if (main[i] == sample[equalIndex])
+		{
+			equalIndex++;
+			i++;
+		}
+		else if (equalIndex == 0)
+		{
+			i++;
+		}
+		else
+		{
+			equalIndex = index[equalIndex - 1];
+		}
+	}
+	delete[] index;
+	return equalIndex;
+}
---
+++ sem1/homework11/11.1/11.1/Seek.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+#include <iostream>
+
+int* prefixFunction(std::string str, int len);
+
+int algorithmKMP(std::string main, std::string sample);
