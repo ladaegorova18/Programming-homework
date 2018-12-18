115acc5d878fd27ab15016944b12c2b3808c2e7d
ladaegorova18 <st068843@student.spbu.ru>
2018-12-18 09:44:25 +0300

    тоже не помню, что изменилось

--- sem1/homework11/Spanning_Tree/Spanning_Tree/Graph.cpp
+++ sem1/homework11/Spanning_Tree/Spanning_Tree/Graph.cpp
@@ -1,6 +1,4 @@
 #include "pch.h"
-#include "pch.h"
-#define _CRT_SECURE_NO_WARNINGS
 #include "Graph.h"
 #include <iterator>
 #include <queue>
@@ -9,7 +7,7 @@ const int INF = 1000000000;
 typedef std::pair<int, int> iPair;
 
 Graph::Graph(int vertices)
-{-
+{
 	this->vert = vertices;
 	branches.resize(vertices);
 	parent.resize(vertices);
