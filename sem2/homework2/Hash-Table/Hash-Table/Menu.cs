753c68b2aeea7fb053d05c5d1310f2f85e85a2e0
ladaegorova18 <st068843@student.spbu.ru>
2019-03-31 22:49:46 +0300

    надеюсь, все будет хорошо

---
+++ sem2/homework2/Hash-Table/Hash-Table/Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Menu
+    {
+        private void TextMenu()
+        {
+            Console.WriteLine("Это меню хэш-таблицы. Нажмите:");
+            Console.WriteLine("1, чтобы добавить указанное слово;");
+            Console.WriteLine("2, чтобы удалить указанное слово из хэш-таблицы;");
+            Console.WriteLine("3, чтобы найти указанное слово;");
+            Console.WriteLine("4, чтобы выйти.");
+        }
+
+        public void WorkWithMenu()
+        {
+            TextMenu();
+            string key = Console.ReadLine();
+            while (key != "4")
+            {
+                Options(key);
+                TextMenu();
+                key = Console.ReadLine();
+            }
+            Console.WriteLine("До свидания!");
+            hashTable.ClearTable();
+        }
+
+        private void Options(string key)
+        {
+            switch (key)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Введите слово для добавления:");
+                        string data = Console.ReadLine();
+                        hashTable.AddData(data);
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Введите слово для удаления:");
+                        string data = Console.ReadLine();
+                        hashTable.RemoveData(data);
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.WriteLine("Введите слово для поиска:");
+                        string data = Console.ReadLine();
+                        if (hashTable.IsData(data))
+                        {
+                            Console.WriteLine("Слово есть в таблице)");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Такого слова нет!");
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        public Menu(HashTable hashTable)
+        {
+            this.hashTable = hashTable;
+        }
+
+        private HashTable hashTable;
+    }
+}
---
+++ sem2/homework2/Hash-Table/Hash-Table/OneLinkedList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace HashTable
+{
+    public class OneLinkedList
+    {
+        public class Node
+        {
+            public string Value { get; set; } = "";
+            public Node Next { get; set; }
+            public Node(string value)
+            {
+                Value = value;
+            }
+        }
+
+        public int Count() => Size;
+
+        public bool IsEmpty() => Size == 0;
+
+        public bool Add(string data)
+        {
+            var newElement = new Node(data);
+            if (IsEmpty())
+            {
+                Head = newElement;
+                Tail = newElement;
+            }
+            else
+            {
+                Tail.Next = newElement;
+                Tail = newElement;
+            }
+            ++Size;
+            return true;
+        }
+
+        public bool Remove(string data)
+        {
+            --Size;
+            if (data == Head.Value && Size == 0)
+            {
+                Head = null;
+                Tail = null;
+                return true;
+            }
+            var temp = Head;
+            Node prev = null;
+            while (temp.Value != data || temp.Next != null)
+            {
+                prev = temp;
+                temp = temp.Next;
+            }
+            if (temp == Tail)
+            {
+                if (temp.Value != data)
+                {
+                    return false;
+                }
+                prev.Next = null;
+                Tail = prev;
+                return true;
+            }
+            prev.Next = temp.Next;
+            return true;
+        }
+
+        public bool Exists(string data)
+        {
+            if (Size == 0)
+            {
+                return false;
+            }
+            var temp = Head;
+            while (temp.Value != data)
+            {
+                if (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return (temp.Value == data);
+        }
+
+        public void ClearList()
+        {
+            Head = null;
+            Tail = null;
+            Size = 0;
+        }
+
+        private int Size { get; set; } = 0;
+        public Node Head { get; set; } = null;
+        public Node Tail { get; set; } = null;
+    }
+}
+
+
