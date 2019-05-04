bc7b8d538ba390e13267533780317ef04aef3239
ladaegorova18 <st068843@student.spbu.ru>
2019-05-04 17:26:34 +0300

    написана большая часть функций, изменен travis.yml

---
+++ sem2/homework8/GenericSet/GenericSet.Tests/SetTests.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericSet.Tests
+{
+    [TestClass]
+    public class SetTests
+    {
+        private Set<int> set;
+        private int[] array;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            set = new Set<int>();
+        }
+
+        [TestMethod]
+        public void AddIntTest()
+        {
+            array = new[] { 5, 6, 7 };
+            set.Add(5);
+            set.Add(6);
+            set.Add(7);
+            var index = 0;
+            foreach(var cell in set)
+            {
+                Assert.AreEqual(array[index], cell);
+            }
+        }
+
+        [TestMethod]
+        public void AddStringTest()
+        {
+        }
+
+        [TestMethod]
+        public void ContainsIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ContainsStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void CopyToTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ExceptWithIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ExceptWithStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IntersectWithIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IntersectWithStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsProperSubsetOfIntTest()
+        {
+            set.Add(1);
+            set.Add(2);
+            set.Add(3);
+            var other = new Set<int>();
+            other.Add(1);
+            other.Add(2);
+            other.Add(3);
+            other.Add(5);
+            var result = set.IsProperSubsetOf(other);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsProperSubsetOfStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsProperSupersetOfIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsProperSupersetOfStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsSubsetOfIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsSubsetOfStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsSupersetOfIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsSupersetOfStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void OverlapsIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void OverlapsStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void RemoveIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void RemoveStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void SetEqualsIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void SetEqualsStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void SymmetricExceptWithIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void SymmetricExceptWithStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void UnionWithIntTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void UnionWithStringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void EnumeratorTest()
+        {
+
+        }
+    }
+}
--- sem2/homework8/GenericSet/GenericSet/Set.cs
+++ sem2/homework8/GenericSet/GenericSet/Set.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GenericSet
 {
@@ -88,12 +86,20 @@ namespace GenericSet
             }
         }
 
+        /// <summary>
+        /// makes set empty
+        /// </summary>
         public void Clear()
         {
             root = null;
             count = 0;
         }
 
+        /// <summary>
+        /// finds value in set
+        /// </summary>
+        /// <param name="item"> value to seek </param>
+        /// <returns> value exists </returns>
         public bool Contains(T item)
         {
             if (count == 0)
@@ -136,46 +142,173 @@ namespace GenericSet
             }
         }
 
+        /// <summary>
+        /// copies set to array
+        /// </summary>
+        /// <param name="array"> array to copy </param>
+        /// <param name="arrayIndex"> start index of array to copy </param>
         public void CopyTo(T[] array, int arrayIndex)
         {
-            var temp = root;
             var index = arrayIndex;
-
+            foreach (var cell in this)
+            {
+                array[index] = cell;
+                ++index;
+            }
         }
 
+        /// <summary>
+        /// removes definite collection from set
+        /// </summary>
+        /// <param name="other"> collection to remove </param>
         public void ExceptWith(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            foreach (var otherCell in other)
+            {
+                Remove(otherCell);
+            }
         }
 
+        /// <summary>
+        /// removes values which are not contained in definite set
+        /// </summary>
+        /// <param name="other"> set to intersect with </param>
         public void IntersectWith(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            foreach (var cell in this)
+            {
+                if (!other.Contains(cell))
+                {
+                    RemoveRecursion(root, cell);
+                }
+            }
         }
 
+        /// <summary>
+        /// checks if set is a proper subset of other
+        /// </summary>
+        /// <param name="other"> other set to check </param>
+        /// <returns> true, if this set is a proper subset </returns>
         public bool IsProperSubsetOf(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            if (Count == 0 || other.Count() == 0)
+            {
+                return true;
+            }
+            foreach (var cell in this)
+            {
+                if (other.Contains(cell))
+                {
+                    continue;
+                }
+                else
+                {
+                    return false; //???????
+                }
+            }
+            foreach (var cell in other)
+            {
+                if (Contains(cell))
+                {
+                    continue;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
+        /// <summary>
+        /// checks if set is a proper superset
+        /// </summary>
+        /// <param name="other"> other set that can be a subset </param>
+        /// <returns> true, if set is a proper superset </returns>
         public bool IsProperSupersetOf(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            if (Count == 0 || other.Count() == 0)
+            {
+                return true;
+            }
+            if (Count < other.Count())
+            {
+                return false;
+            }
+            var thisEnum = GetEnumerator();
+            var otherEnum = GetEnumerator();
+            while (!Equals(thisEnum.Current, otherEnum.Current))
+            {
+                thisEnum.MoveNext();
+            }
+            while (otherEnum.MoveNext())
+            {
+                thisEnum.MoveNext();
+                if (!Equals(thisEnum.Current, otherEnum))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
+        /// <summary>
+        /// if set is a subset
+        /// </summary>
+        /// <param name="other"> superset to check </param>
+        /// <returns> true, if set is a superset </returns>
         public bool IsSubsetOf(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            if (count == 0)
+            {
+                return true;
+            }
+            foreach (var cell in this)
+            {
+                if (!other.Contains(cell))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
+        /// <summary>
+        /// if set is a superset
+        /// </summary>
+        /// <param name="other"> subset to check </param>
+        /// <returns> true, if set is a superset </returns>
         public bool IsSupersetOf(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            if (Count < other.Count())
+            {
+                return false;
+            }
+            foreach (var otherCell in other)
+            {
+                if (!Contains(otherCell))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
+        /// <summary>
+        /// truth, if there are at least one common element
+        /// </summary>
+        /// <param name="other"> other set </param>
+        /// <returns> true, if sets overlap</returns>
         public bool Overlaps(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            foreach (var cell in this)
+            {
+                if (other.Contains(cell))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         /// <summary>
@@ -238,59 +371,98 @@ namespace GenericSet
             return temp.Value;
         }
 
+        /// <summary>
+        /// checks if two sets are equal
+        /// </summary>
+        /// <param name="other"> other set </param>
+        /// <returns> true, if sets are equal </returns>
         public bool SetEquals(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            foreach (var cell in this)
+            {
+                if (!other.Contains(cell))
+                {
+                    return false;
+                }
+            }
+            foreach (var cell in other)
+            {
+                if (!Contains(cell))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
+        /// <summary>
+        /// makes set of unrepeating elements in both sets
+        /// </summary>
+        /// <param name="other"> other set </param>
         public void SymmetricExceptWith(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            foreach (var cell in other)
+            {
+                if (Contains(cell))
+                {
+                    RemoveRecursion(root, cell);
+                }
+                else
+                {
+                    Add(cell);
+                }
+            }
         }
 
+        /// <summary>
+        /// unites two sets
+        /// </summary>
+        /// <param name="other"> second set </param>
         public void UnionWith(IEnumerable<T> other)
         {
-            throw new NotImplementedException();
+            foreach (var cell in other)
+            {
+                Add(cell);
+            }
         }
 
-        void ICollection<T>.Add(T item)
+        void ICollection<T>.Add(T item) => Add(item);
+
+        /// <summary>
+        /// pre-order tree traversal
+        /// </summary>
+        /// <returns> enumerator object </returns>
+        public IEnumerator<T> InOrder()
         {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<T> GetEnum() => new SetEnum(root);
-
-        public IEnumerator<T> GetEnumerator() => GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        private class SetEnum : IEnumerator<T>
-        {
-            private Node root;
-            private Node temp;
-
-            public SetEnum(Node root)
+            if (root == null)
             {
-                this.root = root;
-                temp = root;
+                yield break;
             }
-
-            public T Current => temp.Value;
-
-            object IEnumerator.Current => temp;
-
-            public void Dispose() { }
-
-            public bool MoveNext()
+            var stack = new Stack<Node>();
+            var temp = root;
+            while (stack.Count > 0 && temp != null)
             {
-                throw new NotImplementedException();
-            }
-
-            public void Reset()
-            {
-                temp.leftChild = root;
+                if (temp == null)
+                {
+                    temp = stack.Pop();
+                    yield return temp.Value;
+                    temp = temp.rightChild;
+                }
+                else
+                {
+                    stack.Push(temp);
+                    temp = temp.leftChild;
+                }
             }
         }
+
+        /// <summary>
+        /// gets enumerator to traverse tree
+        /// </summary>
+        /// <returns> enumerator object </returns>
+        public IEnumerator<T> GetEnumerator() => InOrder();
+
+        IEnumerator IEnumerable.GetEnumerator() => InOrder();
     }
 }
 
