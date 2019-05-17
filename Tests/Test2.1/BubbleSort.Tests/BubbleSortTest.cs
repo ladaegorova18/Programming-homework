75029f1c7a1acbaa4feeaa13136d7b61c4f54a51
ladaegorova18 <st068843@student.spbu.ru>
2019-05-17 15:17:12 +0300

    убрано лишнее

--- Tests/Test2.1/BubbleSort.Tests/BubbleSortTest.cs
+++ Tests/Test2.1/BubbleSort.Tests/BubbleSortTest.cs
@@ -62,23 +62,5 @@ namespace BubbleSort.Tests
                 Assert.AreEqual(result[i], list[i]);
             }
         }
-
-        public void DoubleNumberTest()
-        {
-            var doubleComparator = new DoubleComparator();
-            var bubbleSort = new Test2.BubbleSorting<double>();
-            var list = new List<double>();
-            var array = new double[] { 0, -1, 0.3f, 10.4f, 6 };
-            var result = new double[] { -1, 0, 0.3f, 6, 10.4f };
-            foreach (var cell in array)
-            {
-                list.Add(cell);
-            }
-            list = bubbleSort.Sort(list, doubleComparator);
-            for (var i = 0; i < result.Length; ++i)
-            {
-                Assert.AreEqual(result[i], list[i]);
-            }
-        }
     }
 }
