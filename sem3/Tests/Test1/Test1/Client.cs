c69748fa57972cf2368b78b9e27cacb20c51e98c
ladaegorova18 <st068843@student.spbu.ru>
2019-11-01 14:34:42 +0300

    chat version 1.0

---
+++ sem3/Tests/Test1/Test1/Client.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    public class Client
+    {
+        private static AutoResetEvent waitMain = new AutoResetEvent(false);
+
+        public static void Process(int port, string address)
+        {
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient(address, port);
+                var streamWriter = new StreamWriter(client.GetStream()) { AutoFlush = true };
+                var streamReader = new StreamReader(client.GetStream());
+                Reader(streamReader);
+                Writer(streamWriter);
+                waitMain.WaitOne();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private static void Writer(StreamWriter writer)
+        {
+            Task.Run(async () =>
+            {
+                Console.Write("Enter messages to chat: ");
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    if (message != null)
+                    {
+                        await writer.WriteLineAsync(message).ConfigureAwait(false);
+                    }
+                }
+            });
+        }
+
+        private static void Reader(StreamReader reader)
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var response = await reader.ReadLineAsync().ConfigureAwait(false);
+                    if (response != null)
+                    {
+                        Console.WriteLine($"Server requests: {response}");
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ sem3/Tests/Test1/Test1/Server.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    /// <summary>
+    /// server class to handle requests
+    /// </summary>
+    public class Server
+    {
+        /// <summary>
+        /// TcpClient to connect with
+        /// </summary>
+        public TcpClient client { get; private set; }
+
+        /// <summary>
+        /// constructor: assigns this TcpClient
+        /// </summary>
+        public Server(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }
+
+        private readonly AutoResetEvent waitMain = new AutoResetEvent(false);
+
+        /// <summary>
+        /// main server process to read and write to client
+        /// </summary>
+        public void Process()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                var writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+                var reader = new StreamReader(client.GetStream());
+                Reader(reader);
+                Writer(writer);
+                waitMain.WaitOne();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        private static void Writer(StreamWriter writer)
+        {
+            Task.Run(async () =>
+            {
+                Console.Write("Enter messages to chat: ");
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    if (message != null)
+                    {
+                        await writer.WriteLineAsync(message).ConfigureAwait(false);
+                    }
+                }
+            });
+        }
+
+        private static void Reader(StreamReader reader)
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var response = await reader.ReadLineAsync().ConfigureAwait(false);
+                    if (response != null)
+                    {
+                        Console.WriteLine($"Client says: {response}");
+                    }
+                }
+            });
+        }
+    }
+}
