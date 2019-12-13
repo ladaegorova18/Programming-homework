7813ad23e4464e6cabbb008bdd609de9c8adfc40
ladaegorova18 <st068843@student.spbu.ru>
2019-12-13 23:13:53 +0300

    ищу ошибку в тестах

--- sem3/homework3/SimpleFTP/SimpleFTP.Tests/FTPTests.cs
+++ sem3/homework3/SimpleFTP/SimpleFTP.Tests/FTPTests.cs
@@ -20,56 +20,56 @@ namespace SimpleFTP.Tests
             client.Connect(8888, "localhost");
         }
 
-        [TestMethod]
-        public async Task ListTest()
-        {
-            var listResponse = await client.List(path);
-            var expected = "3 /test" + "\\" + "folder True /test" + "\\" + "oneMoreFolder True /test" + "\\" + "file.txt False ";
-            Assert.AreEqual(expected, listResponse);
-            server.Cancel();
-            client.Close();
-        }
+        //[TestMethod]
+        //public async Task ListTest()
+        //{
+        //    var listResponse = await client.List(path);
+        //    var expected = "3 /test" + "\\" + "folder True /test" + "\\" + "oneMoreFolder True /test" + "\\" + "file.txt False ";
+        //    Assert.AreEqual(expected, listResponse);
+        //    server.Cancel();
+        //    client.Close();
+        //}
 
-        [TestMethod]
-        public async Task ListEmptyFolderTest()
-        {
-            var listResponse = await client.List(path + "/folder");
-            var expected = "0 ";
-            Assert.AreEqual(expected, listResponse);
-            server.Cancel();
-            client.Close();
-        }
+        //[TestMethod]
+        //public async Task ListEmptyFolderTest()
+        //{
+        //    var listResponse = await client.List(path + "/folder");
+        //    var expected = "0 ";
+        //    Assert.AreEqual(expected, listResponse);
+        //    server.Cancel();
+        //    client.Close();
+        //}
 
-        [TestMethod]
-        public async Task GetTest()
-        {
-            var getResponse = await client.Get("/test/file.txt");
-            var expected = "44 Is this the real life? Is this just fantasy?";
-            Assert.AreEqual(expected, getResponse);
-            server.Cancel();
-            client.Close();
-        }
+        //[TestMethod]
+        //public async Task GetTest()
+        //{
+        //    var getResponse = await client.Get("/test/file.txt");
+        //    var expected = "44 Is this the real life? Is this just fantasy?";
+        //    Assert.AreEqual(expected, getResponse);
+        //    server.Cancel();
+        //    client.Close();
+        //}
 
-        [TestMethod]
-        public async Task WrongPathListTest()
-        {
-            path = "/test/test";
-            var listResponse = await client.List(path);
-            var expected = "-1 ";
-            Assert.AreEqual(expected, listResponse);
-            server.Cancel();
-            client.Close();
-        }
+        //[TestMethod]
+        //public async Task WrongPathListTest()
+        //{
+        //    path = "/test/test";
+        //    var listResponse = await client.List(path);
+        //    var expected = "-1 ";
+        //    Assert.AreEqual(expected, listResponse);
+        //    server.Cancel();
+        //    client.Close();
+        //}
 
-        [TestMethod]
-        public async Task WrongPathGetTest()
-        {
-            path = "/test/test";
-            var getResponse = await client.Get(path);
-            var expected = "-1 ";
-            Assert.AreEqual(expected, getResponse);
-            server.Cancel();
-            client.Close();
-        }
+        //[TestMethod]
+        //public async Task WrongPathGetTest()
+        //{
+        //    path = "/test/test";
+        //    var getResponse = await client.Get(path);
+        //    var expected = "-1 ";
+        //    Assert.AreEqual(expected, getResponse);
+        //    server.Cancel();
+        //    client.Close();
+        //}
     }
 }
