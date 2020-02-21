032d231c6ff47ab852238434331618d5894d19cc
ladaegorova18 <st068843@student.spbu.ru>
2020-02-21 15:09:10 +0300

    ListOfPows сделан более рационально

--- sem4/Factorial/Factorial/ListOfPows.fs
+++ sem4/Factorial/Factorial/ListOfPows.fs
@@ -1,5 +1,10 @@
 ﻿module ListOfPows
 
+open ListReverse
+
 let listOfPows n m = 
-    if n < 0 || m < 0 then []
-    else List.init m (fun i -> pown 2 (n + i))
+    let rec recListOfPows acc i currPow = 
+        if i < 0 then []
+        else if i = 0 then acc
+        else recListOfPows (currPow :: acc) (i - 1) (currPow * 2)
+    listReverse (recListOfPows [] m (pown 2 n))
--- sem4/Factorial/Factorial/Program.fs
+++ sem4/Factorial/Factorial/Program.fs
@@ -1,6 +1,10 @@
-﻿open System
+﻿open ListOfPows
 
 [<EntryPoint>]
 let main argv =
+    let res = listOfPows 4 6
+    printf "%s" (res.Length.ToString())
+    printfn "%A" (res.ToString())
+    printfn "%d" (res.Item(4))
     printfn "Hello World from F#!"
     0 // return an integer exit code
