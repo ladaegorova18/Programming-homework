4bfe9a11cc629db9c8cd29e92dbdedceef3c43d0
ladaegorova18 <st068843@student.spbu.ru>
2020-02-23 14:11:43 +0300

    firstNumberPosition сделана более быстрой по времени

--- sem4/Factorial/Factorial/FirstNumberPosition.fs
+++ sem4/Factorial/Factorial/FirstNumberPosition.fs
@@ -1,8 +1,9 @@
 ﻿module FirstNumberPosition
 
 let firstNumberPosition n ls = 
-    let rec recPosition n i ls = 
-        if (i = List.length(ls) || ls = []) then None
-        else if (ls.Item(i) = n) then Some(i)
-        else recPosition n (i + 1) ls
-    recPosition n 0 ls
+    let rec recPosition i = 
+        match ls with
+        | [] -> None
+        | head :: tail when n = head -> Some(i)
+        | _ -> recPosition (i + 1)
+    recPosition 0
--- sem4/Factorial/Factorial/Program.fs
+++ sem4/Factorial/Factorial/Program.fs
@@ -1,10 +1,3 @@
-﻿open ListOfPows
+﻿module Program 
 
-[<EntryPoint>]
-let main argv =
-    let res = listOfPows 4 6
-    printf "%s" (res.Length.ToString())
-    printfn "%A" (res.ToString())
-    printfn "%d" (res.Item(4))
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let [<EntryPoint>] main _ = 0
