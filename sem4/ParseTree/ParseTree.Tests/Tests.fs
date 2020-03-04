e7cb3103cad1ebccfcf1b3474d61dae79ab0bb9f
ladaegorova18 <st068843@student.spbu.ru>
2020-03-04 22:37:53 +0300

    загружен проект

---
+++ sem4/ParseTree/ParseTree.Tests/Tests.fs
@@ -0,0 +1,35 @@
+module ParseTree.Tests
+
+open NUnit.Framework
+open Parse
+open FsUnit
+open FsCheck
+
+let testCases =
+    [
+        Sum(Int(3), Int(4)), 7 // 3 + 4
+        Sum(Multiply(Int(2), Int(5)), Int(8)), 18 // 2 * 5 + 8
+        Subtract(Multiply(Int(100), Int(-4)), Int(80)), -480 // 100 * (-4) - 80
+    ] |> List.map (fun (tree, result) -> TestCaseData(tree, result))
+
+[<TestCaseSource("testCases")>]
+[<Test>]
+let ``Parse tree result should be equal expression result custom tests`` tree result = parseTree tree |> should equal result
+
+let ``Sum test`` x y = parseTree (Sum(Int(x), Int(y))) |> should equal (x + y)
+let ``Multiplication test`` x y = parseTree (Multiply(Int(x), Int(y))) |> should equal (x * y)
+let ``Subtraction test`` x y = parseTree (Subtract(Int(x), Int(y))) |> should equal (x - y)
+let ``Division test`` x y = parseTree (Divide(Int(x), Int(y))) |> should equal (x / y)
+
+[<Test>]
+let fsCheckSumTest () = Check.Quick ``Sum test``
+
+[<Test>]
+let fsCheckMultiplyTest () = Check.Quick ``Multiplication test``
+
+[<Test>]
+let fsCheckSubtractionTest () = Check.Quick ``Subtraction test``
+
+[<Test>]
+let fsCheckDivisionTest () = Check.Quick ``Division test``
+
