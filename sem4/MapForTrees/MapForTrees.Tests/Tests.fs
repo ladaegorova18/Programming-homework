83c8564d855e3f239259c264196f4b0919a6d5c9
ladaegorova18 <st068843@student.spbu.ru>
2020-03-05 00:23:01 +0300

    изменены тесты

--- sem4/MapForTrees/MapForTrees.Tests/Tests.fs
+++ sem4/MapForTrees/MapForTrees.Tests/Tests.fs
@@ -7,17 +7,11 @@ open FsUnit
 // test cases for TreeMap tests
 let testCases =
     [
-        (fun x -> x + 1), Tree.Node(5, Tree.Node(3, Tree.Empty, Tree.Empty), Tree.Empty), Tree.Node(6, Tree.Node(4, Tree.Empty, Tree.Empty), Tree.Empty)
-
+        (fun x -> x + 1), Node(5, Node(3, Tree.Empty, Tree.Empty), Tree.Empty), Node(6, Node(4, Tree.Empty, Tree.Empty), Tree.Empty)
+        (fun x -> x * 3), Tree.Empty, Tree.Empty
+        (fun x -> x * 0), Node(400, Node(-400, Tree.Empty, Tree.Empty), Node(9, Tree.Empty, Tree.Empty)), Node(0, Node(0, Tree.Empty, Tree.Empty), Node(0, Tree.Empty, Tree.Empty))
     ] |> List.map (fun (func, tree, resultTree) -> TestCaseData(func, tree, resultTree))
 
 [<TestCaseSource("testCases")>]
 [<Test>]
-let treeMapTest func tree resultTree = 
-    let result = mapTree func tree
-    result |> should equal resultTree
-
-[<Test>]
-let secondTest x = 
-    let result = mapTree (fun x -> x + 1) (Tree.Node(5, Tree.Node(3, Tree.Empty, Tree.Empty), Tree.Empty))
-    result |>  should equal (Tree.Node(6, Tree.Node(4, Tree.Empty, Tree.Empty), Tree.Empty))
+let treeMapTest (func: (int -> int), tree: Tree<int>, resultTree: Tree<int>) = mapTree func tree |> should equal resultTree
