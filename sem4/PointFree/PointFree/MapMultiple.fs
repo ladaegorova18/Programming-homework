3a30a4d1e83de967e582b51108b1e066d89dcc9b
ladaegorova18 <st068843@student.spbu.ru>
2020-04-04 23:57:18 +0300

    добавлены тесты

---
+++ sem4/PointFree/PointFree/MapMultiple.fs
@@ -0,0 +1,12 @@
+﻿module MapMultiple
+
+//takes list and number and multiples all elements to number
+let mapMultiple1 x l = List.map (fun y -> y * x) l
+
+let mapMultiple2 (x: int) (l: int list) = List.map (fun y -> y * x) l
+
+let mapMultiple3 x: int list -> int list = List.map (fun y -> y * x)
+
+let mapMultiple4 x: int list -> int list = List.map ((*) x)
+
+let mapMultiple5: int -> int list -> int list = List.map << (*)
