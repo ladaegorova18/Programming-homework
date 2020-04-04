56968ac51f093bddfc19d1c747e07bc935bafa32
ladaegorova18 <st068843@student.spbu.ru>
2020-04-04 23:25:19 +0300

    добавлен метод BracketsChecker

--- sem4/BracketsChecker/BracketsChecker/Checker.fs
+++ sem4/BracketsChecker/BracketsChecker/Checker.fs
@@ -1,4 +1,23 @@
 ﻿module Checker
 
+open Stack
+
+// Checks a bracket balance in line
+let oneTypeBracketsChecker (openBracket, closeBracket, equals, line) =
+    let rec recChecker lineToCheck (stack : MyStack) =
+        match lineToCheck with
+        | [] -> stack.EmptyStack
+        | x :: tail -> 
+            if (equals openBracket x) then
+                recChecker tail (stack.Push x)
+            elif not stack.EmptyStack
+                && equals closeBracket x && equals stack.Top openBracket then
+                    recChecker tail stack.Pop
+            else false
+    recChecker (Seq.toList(line)) Empty
+
 let bracketsChecker line =
-    
+    //oneTypeBracketsChecker ('{', '}', (=), line) &&
+    oneTypeBracketsChecker ('(', ')', (=), line) &&
+    oneTypeBracketsChecker ('[', ']', (=), line) 
+
---
+++ sem4/BracketsChecker/BracketsChecker/Stack.fs
@@ -0,0 +1,23 @@
+﻿module Stack
+
+// Stack to collect char values
+type MyStack =
+    | MyStack of char * MyStack
+    | Empty
+    with 
+     member stack.Top  =
+         match stack with
+         | MyStack(hd, _) -> hd
+         | Empty -> invalidOp "stack is empty!"
+
+     member stack.Push element = MyStack(element, stack)
+
+     member stack.Pop =
+         match stack with 
+             | MyStack(_, tl) -> tl
+             | Empty -> invalidOp "stack is empty!"
+
+     member stack.EmptyStack =
+         match stack with
+         | MyStack(hd, _)-> false
+         | Empty -> true
