27dc310d03269f0d2a44a13316b25c53e9af6134
ladaegorova18 <st068843@student.spbu.ru>
2020-06-04 15:38:10 +0300

    написаны тесты

---
+++ sem4/WebDownloader/WebDownloader/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+open Downloader
+
+/// Вводим адрес с консоли и скачиваем все страницы, на которые ссылается страница по этому адресу
+[<EntryPoint>]
+let main argv =
+    printfn "Введите адрес:"
+    let address = Console.ReadLine()
+    let content = address |> fetchAsync |> Async.RunSynchronously
+
+    let links = findLinks content.Value
+    do printfn "This page has %d links" <| links.Length
+
+    let result = getAllPages content
+    List.iter (fun x -> printfn "%s" x) result
+    0
