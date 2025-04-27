53d1de6d30fc68f65addf1790c124d06c411a066
Lada Egorova <43664560+ladaegorova18@users.noreply.github.com>
2025-04-27 12:18:28 +0200

    Update Tests.fs

--- sem4/InfinitePrimes/InfinitePrimes.Tests/Tests.fs
+++ sem4/InfinitePrimes/InfinitePrimes.Tests/Tests.fs
@@ -5,14 +5,15 @@ open CreateSeq
 open FsUnit
 
 let testCases =
-    [
-        2, 0
-        3, 1
-        5, 2
-        7, 3
-        11, 4
-        13, 5
-    ] |> List.map (fun (number, pos) -> TestCaseData(number, pos))
+        [
+            (2, 0)
+            (3, 1)
+            (5, 2)
+            (7, 3)
+            (11, 4)
+            (13, 5)
+        ]
+        |> List.map (fun (number, pos) -> TestCaseData(number, pos))
 
 [<TestCaseSource("testCases")>]
 [<Test>]
